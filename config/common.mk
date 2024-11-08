1c251a27b70f27a1191e01d42902b69cc3f634b8
Drenzzz <realdrenzzz@gmail.com>
2024-11-08 19:35:19 +0100

    config: Compile GameSpace

--- config/common.mk
+++ config/common.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/pixelage/prebuilt/common/etc/init/init.pixelage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelage-updater.rc
 
+# Gamespace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \
